d8a3ef70bd94e1c5de93365bbd4b975d99243b9c
xforce35 <xforce35@hotmail.fr>
2022-03-07 10:41:23 +0100

    essai

--- public/script/templates/ImageLightbox.js
+++ public/script/templates/ImageLightbox.js
@@ -1,7 +1,7 @@
 export default class ImageLightBox {
     constructor({title, src}) {
         const content = `
-            <img class="works-lightbox-media"src="../public/assets/media/${src}" alt="${title}"></img>
+            <img class="works-lightbox-media"src="public/assets/media/${src}" alt="${title}"></img>
             <div class="works-lightbox-media-name">${title}</div>
         `
         return document.createRange().createContextualFragment(content);
--- public/script/templates/VideoLightbox.js
+++ public/script/templates/VideoLightbox.js
@@ -1,7 +1,7 @@
 export default class VideoLightBox {
     constructor({title, src}) {
         const content = `            
-            <video controls class="works-lightbox-video" src="../public/assets/media/${src}" alt="${title}" type="video/mp4"></video>
+            <video controls class="works-lightbox-video" src="public/assets/media/${src}" alt="${title}" type="video/mp4"></video>
             <div class="works-lightbox-video-name">${title}</div>
         `
 
