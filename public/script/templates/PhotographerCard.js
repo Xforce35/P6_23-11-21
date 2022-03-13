1cc7a5a6d21b1cc0bb08eccc6788d9aa41596f47
xforce35 <xforce35@hotmail.fr>
2022-03-13 03:06:50 +0100

    derniere version

--- public/script/templates/PhotographerCard.js
+++ public/script/templates/PhotographerCard.js
@@ -8,13 +8,13 @@ class PhotographerCard {
 
         const photographerCard = `
         <article class="photographer_section_profile">
-            <a class="photographer_section_profile_link" href="pages/photographer.html?id=${this._index.id}">
+            <a class="photographer_section_profile_link" href="../pages/photographer.html?id=${this._index.id}">
                 <img src="${this._index.picture}" class="photographer_section_profile_link_picture" alt="Portrait de ${this._index.name}"></img>
                 <h2 class="photographer_section_profile_link_name">${this._index.name}</h2>
             </a>
             <p class="photographer_section_profile_location" role="text" aria-label="Lieu de résidence du photographe">${this._index.city}, ${this._index.country}</p>
             <p class="photographer_section_profile_description" role="text" aria-label="Phare d'accroche du photographe">${this._index.tagline}</p>
-            <p class="photographer_section_profile_price" role="text" aria-label="Prix du photographe: 250 par jour">${this._index.price}€/jour</p>
+            <p class="photographer_section_profile_price" role="text" aria-label="Honoraire du photographe par jour">${this._index.price}€/jour</p>
         </article>
         `
 
